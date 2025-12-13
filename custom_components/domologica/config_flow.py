30cb9eb392ab23c79086c1bf96d4a11bbf615c75
matteopala <matteopala@gmail.com>
2025-12-13 17:28:14 +0100

    fix

--- custom_components/domologica/config_flow.py
+++ custom_components/domologica/config_flow.py
@@ -1,20 +1,39 @@
 from homeassistant import config_entries
 import voluptuous as vol
 
-from .const import DOMAIN, CONF_URL, CONF_USERNAME, CONF_PASSWORD, CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+from .const import (
+    DOMAIN,
+    CONF_URL,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_SCAN_INTERVAL,
+    DEFAULT_SCAN_INTERVAL,
+)
+
 
 class DomologicaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-VERSION = 1
+    """Config flow for Domologica integration."""
 
-async def async_step_user(self, user_input=None):
-if user_input is not None:
-return self.async_create_entry(title="Domologica", data=user_input)
+    VERSION = 1
 
-schema = vol.Schema({
-vol.Required(CONF_URL): str,
-vol.Required(CONF_USERNAME): str,
-vol.Required(CONF_PASSWORD): str,
-vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): int,
-})
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Domologica",
+                data=user_input,
+            )
 
-return self.async_show_form(step_id="user", data_schema=schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_URL): str,
+                    vol.Required(CONF_USERNAME): str,
+                    vol.Required(CONF_PASSWORD): str,
+                    vol.Optional(
+                        CONF_SCAN_INTERVAL,
+                        default=DEFAULT_SCAN_INTERVAL,
+                    ): int,
+                }
+            ),
+        )
