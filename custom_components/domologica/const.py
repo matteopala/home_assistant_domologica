dab72b9b55743b719d6f9776efaff0f8be924f68
matteopala <matteopala@gmail.com>
2025-12-13 22:04:02 +0100

    1

--- custom_components/domologica/const.py
+++ custom_components/domologica/const.py
@@ -2,7 +2,7 @@ DOMAIN = "domologica"
 CONF_URL = "url"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
-PLATFORMS = ["light"]
+PLATFORMS = ["light", "sensor"]
 
 import logging
 _LOGGER = logging.getLogger(__name__)
