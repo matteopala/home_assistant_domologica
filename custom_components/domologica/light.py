7151ab0b8017f59571c9a093bbdc08ca74d83953
matteopala <matteopala@gmail.com>
2025-12-14 08:42:27 +0100

    light turn on v2

--- custom_components/domologica/light.py
+++ custom_components/domologica/light.py
@@ -1,62 +1,50 @@
 import logging
+import aiohttp
+
 from homeassistant.components.light import (
     LightEntity,
     ColorMode,
 )
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from aiohttp import BasicAuth
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-    async_add_entities,
-):
-    """Setup delle luci Domologica."""
-    coordinator = hass.data[DOMAIN][entry.entry_id]["coordinator"]
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Setup luci Domologica."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
 
     entities = []
 
     for element_path, statuses in coordinator.data.items():
+        # Consideriamo luce solo se ha switched on/off
         if "isswitchedon" in statuses or "isswitchedoff" in statuses:
             entities.append(
                 DomologicaLight(
                     coordinator=coordinator,
                     element_path=element_path,
-                    base_url=entry.data["url"],
-                    username=entry.data["username"],
-                    password=entry.data["password"],
+                    statuses=statuses,
                 )
             )
 
     async_add_entities(entities)
 
 
-class DomologicaLight(LightEntity):
-    """Luce Domologica (on/off o dimmer)."""
+class DomologicaLight(CoordinatorEntity, LightEntity):
+    """Entità luce Domologica."""
 
-    def __init__(
-        self,
-        coordinator,
-        element_path: str,
-        base_url: str,
-        username: str,
-        password: str,
-    ):
-        self.coordinator = coordinator
+    def __init__(self, coordinator, element_path, statuses):
+        super().__init__(coordinator)
+
         self.element_path = element_path
-        self.base_url = base_url.rstrip("/")
-        self._auth = BasicAuth(username, password)
+        self.statuses = statuses
 
         self._attr_unique_id = f"domologica_light_{element_path}"
 
-        if self._has_dimmer:
+        # Nome: dimmer o light
+        if "getdimmer" in statuses:
             self._attr_name = f"Domologica Dimmer {element_path}"
             self._attr_supported_color_modes = {ColorMode.BRIGHTNESS}
             self._attr_color_mode = ColorMode.BRIGHTNESS
@@ -66,57 +54,46 @@ class DomologicaLight(LightEntity):
             self._attr_color_mode = ColorMode.ONOFF
 
     @property
-    def _has_dimmer(self) -> bool:
-        data = self.coordinator.data.get(self.element_path, {})
-        return "Getdimmer" in data
+    def is_on(self):
+        """Stato ON/OFF."""
+        return "isswitchedon" in self.coordinator.data.get(self.element_path, {})
 
     @property
-    def is_on(self) -> bool | None:
-        data = self.coordinator.data.get(self.element_path)
-        if not data:
-            return None
-        return "isswitchedon" in data
-
-    @property
-    def brightness(self) -> int | None:
-        if not self._has_dimmer:
-            return None
-
-        value = self.coordinator.data[self.element_path].get("Getdimmer")
-        if value is None:
-            return None
-
-        return int(value * 255 / 100)
-
-    @property
-    def extra_state_attributes(self):
-        return self.coordinator.data.get(self.element_path, {})
+    def brightness(self):
+        """Luminosità (0–255)."""
+        statuses = self.coordinator.data.get(self.element_path, {})
+        if "getdimmer" in statuses:
+            try:
+                value = int(statuses["getdimmer"])
+                return int(value * 255 / 100)
+            except Exception:
+                return None
+        return None
 
     async def async_turn_on(self, **kwargs):
         """Accende la luce."""
-        url = f"{self.base_url}/elements/{self.element_path}.xml?action=switchon"
-        session = async_get_clientsession(self.hass)
-
-        _LOGGER.debug("Domologica ON: %s", url)
-
-        async with session.get(url, auth=self._auth) as resp:
-            if resp.status != 200:
-                raise Exception(f"Errore accensione Domologica {self.element_path}")
-
+        await self._send_command("switchedon")
         await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs):
         """Spegne la luce."""
-        url = f"{self.base_url}/elements/{self.element_path}.xml?action=switchoff"
-        session = async_get_clientsession(self.hass)
-
-        _LOGGER.debug("Domologica OFF: %s", url)
-
-        async with session.get(url, auth=self._auth) as resp:
-            if resp.status != 200:
-                raise Exception(f"Errore spegnimento Domologica {self.element_path}")
-
+        await self._send_command("switchoff")
         await self.coordinator.async_request_refresh()
 
-    async def async_update(self):
-        await self.coordinator.async_request_refresh()
+    async def _send_command(self, action):
+        """Invia comando HTTP a Domologica."""
+        url = f"{self.coordinator.url}/elements/{self.element_path}.xml?action={action}"
+
+        auth = aiohttp.BasicAuth(
+            self.coordinator.username,
+            self.coordinator.password,
+        )
+
+        _LOGGER.debug("Domologica call: %s", url)
+
+        async with aiohttp.ClientSession(auth=auth) as session:
+            async with session.get(url, timeout=10) as resp:
+                if resp.status != 200:
+                    raise Exception(
+                        f"Domologica HTTP error {resp.status} for {url}"
+                    )
